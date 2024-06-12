8948ebf5ce28e531c1dd1d0c21189bf5cffc9d58
finma <firmanmaulana2403@gmail.com>
2024-06-12 19:03:41 +0700

    [CHG] update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,38 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)",
-  "/protected(.*)",
-]);
+const isProtectedRoute = createRouteMatcher(["/organization(.*)"]);
 
 export default clerkMiddleware(
   (auth, req) => {
-    if (isProtectedRoute(req)) auth().protect();
+    if (isProtectedRoute(req)) {
+      auth().protect();
+    } else if (auth().userId && !isProtectedRoute(req)) {
+      let path = "/select-org";
+
+      if (auth().orgId) {
+        path = `/organization/${auth().orgId}`;
+      }
+
+      // Check if we're not already on the redirect target
+      if (req.nextUrl.pathname !== path) {
+        const orgSelection = new URL(path, req.url);
+        return NextResponse.redirect(orgSelection);
+      }
+    }
+
+    // if (!auth().userId && !isProtectedRoute(req)) {
+    //   return auth().redirectToSignIn({ returnBackUrl: req.url });
+    // }
+
+    // if (
+    //   auth().userId &&
+    //   !auth().orgId &&
+    //   req.nextUrl.pathname !== "/select-org"
+    // ) {
+    //   const orgSelection = new URL("/select-org", req.url);
+    //   return NextResponse.redirect(orgSelection);
+    // }
   }
   // { debug: true }
 );
