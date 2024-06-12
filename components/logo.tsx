9db5e4b192b11061de8f174f817398466f67fb7b
finma <firmanmaulana2403@gmail.com>
2024-06-12 14:41:40 +0700

    [CHG] update source local font

--- components/logo.tsx
+++ components/logo.tsx
@@ -4,7 +4,7 @@ import localFont from "next/font/local";
 import { cn } from "@/lib/utils";
 
 const headingFont = localFont({
-  src: "../../public/fonts/font.woff2",
+  src: "../public/fonts/font.woff2",
 });
 
 export const Logo = () => {
