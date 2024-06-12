b065ffb5bc5dc07a687b0acfa60aab0993d3c5a0
finma <firmanmaulana2403@gmail.com>
2024-06-12 14:17:49 +0700

    [ADD] marketing page

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,55 @@
+import { Medal } from "lucide-react";
+import Link from "next/link";
+import localFont from "next/font/local";
+import { Poppins } from "next/font/google";
+
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+const headingFont = localFont({
+  src: "../../public/fonts/font.woff2",
+});
+
+const textFont = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+const MarketingPage = () => {
+  return (
+    <div className="flex items-center justify-center flex-col">
+      <div
+        className={cn(
+          "flex items-center justify-center flex-col",
+          headingFont.className
+        )}
+      >
+        <div className="flex items-center border bg-amber-100 text-amber-700 rounded-full p-4 shadow-sm mb-4 uppercase">
+          <Medal className="size-6 mr-2" />
+          No 1 Task Management
+        </div>
+        <h1 className="text-3xl md:text-6xl text-neutral-800 text-center mb-6">
+          Taskify helps team move
+        </h1>
+        <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white p-2 px-4 rounded-md w-fit">
+          work forward.
+        </div>
+      </div>
+      <div
+        className={cn(
+          "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
+          textFont.className
+        )}
+      >
+        Collaborate, manage projects, and reach new productivity peaks. From
+        high rises to the home office, the way your team work is unique -
+        accomplish it all wist Taskify.
+      </div>
+      <Button className="mt-6" size="lg" asChild>
+        <Link href="/sign-up">Get Taskify for free</Link>
+      </Button>
+    </div>
+  );
+};
+
+export default MarketingPage;
