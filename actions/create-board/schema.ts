610b30baba3fe37fb49363529edb330bcfab7bf0
finma <firmanmaulana2403@gmail.com>
2024-06-13 21:43:40 +0700

    [ADD] types & schema for create board

---
+++ actions/create-board/schema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const CreateBoard = z.object({
+  title: z
+    .string({
+      required_error: "Title is required",
+      invalid_type_error: "Title is required",
+    })
+    .min(1, {
+      message: "Title is too short",
+    }),
+});
