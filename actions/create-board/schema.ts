892fe828de3640b4bc158a0a9d590b8920d8f4bb
finma <firmanmaulana2403@gmail.com>
2024-06-14 09:47:21 +0700

    [CHG] update minimum string title to 3

--- actions/create-board/schema.ts
+++ actions/create-board/schema.ts
@@ -6,7 +6,7 @@ export const CreateBoard = z.object({
       required_error: "Title is required",
       invalid_type_error: "Title is required",
     })
-    .min(1, {
+    .min(3, {
       message: "Title is too short",
     }),
 });
