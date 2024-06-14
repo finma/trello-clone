29f721007e22e13ec0811d56ead0886667c545ab
finma <firmanmaulana2403@gmail.com>
2024-06-14 20:02:57 +0700

    [ADD] hint component

---
+++ components/hint.tsx
@@ -0,0 +1,31 @@
+import { TooltipProvider } from "@radix-ui/react-tooltip";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
+
+interface HintProps {
+  children: React.ReactNode;
+  description: string;
+  side?: "top" | "bottom" | "left" | "right";
+  sideOffset?: number;
+}
+
+export const Hint = ({
+  children,
+  description,
+  side = "bottom",
+  sideOffset = 0,
+}: HintProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={0}>
+        <TooltipTrigger>{children}</TooltipTrigger>
+        <TooltipContent
+          side={side}
+          sideOffset={sideOffset}
+          className="text-xs max-w-[220px] break-words"
+        >
+          {description}
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
