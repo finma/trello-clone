933f75587caae6a3b07976a939021ae6a9ad3e2b
finma <firmanmaulana2403@gmail.com>
2024-06-15 09:28:03 +0700

    [ADD] form picker for image

---
+++ components/form/form-picker.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import { Check, Loader2 } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useFormStatus } from "react-dom";
+import Image from "next/image";
+import Link from "next/link";
+
+import { unsplash } from "@/lib/unsplash";
+import { cn } from "@/lib/utils";
+import { defaultImages } from "@/constants/images";
+
+interface FormPickerProps {
+  id: string;
+  errors?: Record<string, string[] | undefined>;
+}
+
+export const FormPicker = ({ id, errors }: FormPickerProps) => {
+  const { pending } = useFormStatus();
+
+  const [images, setImages] =
+    useState<Array<Record<string, any>>>(defaultImages);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [selectedImageId, setSelectedImageId] = useState(null);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const result = await unsplash.photos.getRandom({
+          collectionIds: ["317099"],
+          count: 9,
+        });
+
+        if (result && result.response) {
+          const newImages = result.response as Array<Record<string, any>>;
+          setImages(newImages);
+        } else {
+          console.error("Failed to get images from Unsplash!");
+        }
+      } catch (error) {
+        console.log(error);
+        setImages(defaultImages);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchImages();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center p-6">
+        <Loader2 className="size-6 text-sky-600 animate-spin" />
+      </div>
+    );
+  }
+
+  return (
+    <div className="relative">
+      <div className="grid grid-cols-3 gap-2 mb-2">
+        {images.map((image) => (
+          <div
+            key={image.id}
+            onClick={() => {
+              if (pending) return;
+              setSelectedImageId(image.id);
+            }}
+            className={cn(
+              "cursor-pointer aspect-video relative hover:opacity-75 transition bg-muted group",
+              pending && "opacity-50 hover:opacity-50 cursor-auto"
+            )}
+          >
+            <input
+              type="radio"
+              id={id}
+              name={id}
+              checked={selectedImageId === image.id}
+              disabled={pending}
+              className="hidden"
+              readOnly
+              value={`${image.id}|${image.urls.thumb}|${image.urls.full}|${image.links.html}|${image.user.name}`}
+            />
+            <Image
+              fill
+              src={image.urls.thumb}
+              alt="Unsplash image"
+              className="object-cover rounded-sm"
+            />
+            {selectedImageId === image.id && (
+              <div className="absolute size-full bg-black/30 flex items-center justify-center rounded-sm">
+                <Check className="size-6 text-white" />
+              </div>
+            )}
+            <Link
+              href={image.links.html}
+              target="_blank"
+              className="opacity-0 group-hover:opacity-100 absolute bottom-0 w-full text-[10px] truncate text-white p-1 bg-black/50 hover:underline"
+            >
+              {image.user.name}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
