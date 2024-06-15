aaf70721764efae9732f2d793d418bd1959618a1
finma <firmanmaulana2403@gmail.com>
2024-06-15 09:27:42 +0700

    [ADD] new protected route for board

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(["/organization(.*)"]);
+const isProtectedRoute = createRouteMatcher([
+  "/organization(.*)",
+  "/board(.*)",
+]);
 
 export default clerkMiddleware(
   (auth, req) => {
