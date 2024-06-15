5d2348dafce7ce4295022d45edfa53ec21cb8fcf
finma <firmanmaulana2403@gmail.com>
2024-06-15 10:29:46 +0700

    [ADD] board id page with layout

---
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -0,0 +1,65 @@
+import { auth } from "@clerk/nextjs/server";
+import { notFound, redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { boardId: string };
+}) {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    return {
+      title: "Board",
+    };
+  }
+
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+
+  return {
+    title: board?.title || "Board",
+  };
+}
+
+const BoardIdLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { boardId: string };
+}) => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+
+  if (!board) {
+    notFound();
+  }
+
+  return (
+    <div
+      style={{ backgroundImage: `url(${board.imageFullUrl})` }}
+      className="h-full bg-cover bg-center bg-no-repeat relative"
+    >
+      <main className="relative h-full pt-28">{children}</main>
+    </div>
+  );
+};
+
+export default BoardIdLayout;
