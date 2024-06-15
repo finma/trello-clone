0dd8b3791c568f912406bdc728e65eea173ce2f4
finma <firmanmaulana2403@gmail.com>
2024-06-15 11:49:02 +0700

    [ADD] schema, types, & action to delete board

---
+++ actions/delete-board/index.ts
@@ -0,0 +1,44 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+
+import { createSafeAction } from "@/lib/create-safe-action";
+import { db } from "@/lib/db";
+
+import { DeleteBoard } from "./schema";
+import { InputType, ReturnType } from "./types";
+import { redirect } from "next/navigation";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  const { id } = data;
+
+  // let board;
+
+  try {
+    await db.board.delete({
+      where: {
+        id,
+        orgId,
+      },
+    });
+  } catch (error) {
+    return {
+      error: "Failed to delete board",
+    };
+  }
+
+  revalidatePath(`/organization/${orgId}`);
+  redirect(`/organization/${orgId}`);
+  // return { data: board };
+};
+
+export const deleteBoard = createSafeAction(DeleteBoard, handler);
