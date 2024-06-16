9eab123e7303fb10c7d577b8d418b4de665e7ec5
finma <firmanmaulana2403@gmail.com>
2024-06-16 22:46:05 +0700

    [ADD] schema, types, & action to delete list

---
+++ actions/delete-list/index.ts
@@ -0,0 +1,45 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+
+import { createSafeAction } from "@/lib/create-safe-action";
+import { db } from "@/lib/db";
+
+import { DeleteList } from "./schema";
+import { InputType, ReturnType } from "./types";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  const { id, boardId } = data;
+
+  let list;
+
+  try {
+    list = await db.list.delete({
+      where: {
+        id,
+        boardId,
+        board: {
+          orgId,
+        },
+      },
+    });
+  } catch (error) {
+    return {
+      error: "Failed to delete list",
+    };
+  }
+
+  revalidatePath(`/board/${boardId}`);
+  return { data: list };
+};
+
+export const deleteList = createSafeAction(DeleteList, handler);
