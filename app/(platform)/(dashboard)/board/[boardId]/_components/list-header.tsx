6bb797a69560be1d87775022fb00c8a3628e9c1f
finma <firmanmaulana2403@gmail.com>
2024-06-16 22:06:07 +0700

    [ADD] list header component with form to update list title

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import { List } from "@prisma/client";
+import { ElementRef, useRef, useState } from "react";
+import { toast } from "sonner";
+import { useEventListener } from "usehooks-ts";
+
+import { updateList } from "@/actions/update-list";
+import { FormInput } from "@/components/form/form-input";
+import { useAction } from "@/hooks/use-action";
+
+interface ListHeaderProps {
+  list: List;
+}
+
+export const ListHeader = ({ list }: ListHeaderProps) => {
+  const formRef = useRef<ElementRef<"form">>(null);
+  const inputRef = useRef<ElementRef<"input">>(null);
+
+  const [title, setTitle] = useState(list.title);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+      inputRef.current?.select();
+    });
+  };
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const { execute } = useAction(updateList, {
+    onSuccess: (data) => {
+      toast.success(`List "${data.title}" updated!`);
+      setTitle(data.title);
+      disableEditing();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    const id = formData.get("id") as string;
+    const boardId = formData.get("boardId") as string;
+
+    if (title === list.title) {
+      return disableEditing();
+    }
+
+    execute({ id, boardId, title });
+  };
+
+  const onBlur = () => {
+    formRef.current?.requestSubmit();
+  };
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      formRef.current?.requestSubmit();
+    }
+  };
+
+  useEventListener("keydown", onKeyDown);
+
+  return (
+    <div className=" text-sm font-semibold flex justify-between items-start gap-x-2">
+      {isEditing ? (
+        <form ref={formRef} action={onSubmit} className="flex-1 px-[2px]">
+          <input hidden id="id" name="id" value={list.id} />
+          <input hidden id="boardId" name="boardId" value={list.boardId} />
+          <FormInput
+            id="title"
+            ref={inputRef}
+            onBlur={onBlur}
+            defaultValue={title}
+            placeholder="Enter list title..."
+            className="text-sm px-[7px] py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition truncate bg-transparent focus:bg-white"
+          />
+          <button type="submit" hidden />
+        </form>
+      ) : (
+        <div
+          onClick={enableEditing}
+          className="w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent"
+        >
+          {title}
+        </div>
+      )}
+    </div>
+  );
+};
