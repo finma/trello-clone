7581cb9cbad7dd9b8118323f99dc3d7ea0c29984
finma <firmanmaulana2403@gmail.com>
2024-06-17 16:30:22 +0700

    [ADD] schema, types, & action to update list order

---
+++ actions/update-list-order/index.ts
@@ -0,0 +1,48 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+
+import { createSafeAction } from "@/lib/create-safe-action";
+import { db } from "@/lib/db";
+
+import { UpdateListOrder } from "./schema";
+import { InputType, ReturnType } from "./types";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  const { items, boardId } = data;
+
+  let lists;
+
+  try {
+    const transaction = items.map((list) =>
+      db.list.update({
+        where: {
+          id: list.id,
+        },
+        data: {
+          order: list.order,
+        },
+      })
+    );
+
+    lists = await db.$transaction(transaction);
+  } catch (error) {
+    return {
+      error: "Failed to reorder list",
+    };
+  }
+
+  revalidatePath(`/board/${boardId}`);
+  return { data: lists };
+};
+
+export const updateListOrder = createSafeAction(UpdateListOrder, handler);
