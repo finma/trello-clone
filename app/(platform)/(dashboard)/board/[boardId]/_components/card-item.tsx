e268f47724657b35101bca4a24e245b9aeb4e851
finma <firmanmaulana2403@gmail.com>
2024-06-17 20:39:21 +0700

    [ADD] hook to open card modal

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -3,12 +3,16 @@
 import { Card } from "@prisma/client";
 import { Draggable } from "@hello-pangea/dnd";
 
+import { useCardModal } from "@/hooks/use-card-modal";
+
 interface CardItemProps {
   index: number;
   card: Card;
 }
 
 export const CardItem = ({ index, card }: CardItemProps) => {
+  const cardModal = useCardModal();
+
   return (
     <Draggable draggableId={card.id} index={index}>
       {(provided) => (
@@ -17,6 +21,7 @@ export const CardItem = ({ index, card }: CardItemProps) => {
           {...provided.dragHandleProps}
           ref={provided.innerRef}
           role="button"
+          onClick={() => cardModal.onOpen(card.id)}
           className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white shadow-sm rounded-md"
         >
           {card.title}
