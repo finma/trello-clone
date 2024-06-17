a07f155b937d70ec1a9c89318852890edaf08325
finma <firmanmaulana2403@gmail.com>
2024-06-17 21:46:41 +0700

    [ADD] query provider

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -2,13 +2,16 @@ import { Toaster } from "sonner";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import { ModalProvider } from "@/components/providers/modal-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
-      <Toaster />
-      <ModalProvider />
-      {children}
+      <QueryProvider>
+        <Toaster />
+        <ModalProvider />
+        {children}
+      </QueryProvider>
     </ClerkProvider>
   );
 };
