d25882591a04029f7eaa8cebdd5adc4825ba85c4
finma <firmanmaulana2403@gmail.com>
2024-06-18 21:05:59 +0700

    [ADD] action component & description component to card modal component

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -8,6 +8,8 @@ import { useCardModal } from "@/hooks/use-card-modal";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 
 import { Header } from "./header";
+import { Action } from "./action";
+import { Description } from "./description";
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id);
@@ -23,6 +25,20 @@ export const CardModal = () => {
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent>
         {!cardData ? <Header.Skeleton /> : <Header card={cardData} />}
+
+        <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
+          <div className="col-span-3">
+            <div className="w-full space-y-6">
+              {!cardData ? (
+                <Description.Skeleton />
+              ) : (
+                <Description card={cardData} />
+              )}
+            </div>
+          </div>
+
+          {!cardData ? <Action.Skeleton /> : <Action card={cardData} />}
+        </div>
       </DialogContent>
     </Dialog>
   );
