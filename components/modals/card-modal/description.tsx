0c8816aa4de6f5ef88456b112e2527d43a58b9e8
finma <firmanmaulana2403@gmail.com>
2024-06-18 21:05:08 +0700

    [ADD] description component with action to update description

---
+++ components/modals/card-modal/description.tsx
@@ -0,0 +1,123 @@
+"use client";
+
+import { toast } from "sonner";
+import { AlignLeft } from "lucide-react";
+import { useParams } from "next/navigation";
+import { ElementRef, useRef, useState } from "react";
+import { useQueryClient } from "@tanstack/react-query";
+import { useEventListener, useOnClickOutside } from "usehooks-ts";
+
+import { CardWithList } from "@/types";
+import { useAction } from "@/hooks/use-action";
+import { Button } from "@/components/ui/button";
+import { updateCard } from "@/actions/update-card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { FormSubmit } from "@/components/form/form-submit";
+import { FormTextarea } from "@/components/form/form-textarea";
+
+interface DescriptionProps {
+  card: CardWithList;
+}
+
+export const Description = ({ card }: DescriptionProps) => {
+  const params = useParams();
+  const queryClient = useQueryClient();
+
+  const formRef = useRef<ElementRef<"form">>(null);
+  const textareaRef = useRef<ElementRef<"textarea">>(null);
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      textareaRef.current?.focus();
+    });
+  };
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      disableEditing();
+    }
+  };
+
+  useEventListener("keydown", onKeyDown);
+  useOnClickOutside(formRef, disableEditing);
+
+  const { execute, fieldErrors } = useAction(updateCard, {
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["card", card.id] });
+      toast.success(`Card "${data.title}" updated!`);
+      disableEditing();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const onSubmit = (formData: FormData) => {
+    const description = formData.get("description") as string;
+    const boardId = params.boardId as string;
+
+    execute({
+      id: card.id,
+      description,
+      boardId,
+    });
+  };
+
+  return (
+    <div className="flex items-start gap-x-3 w-full">
+      <AlignLeft className="size-5 mt-0.5 text-neutral-700" />
+      <div className="w-full">
+        <p className="font-semibold text-neutral-700 mb-2">Description</p>
+        {isEditing ? (
+          <form ref={formRef} action={onSubmit} className="space-y-2">
+            <FormTextarea
+              id="description"
+              ref={textareaRef}
+              errors={fieldErrors}
+              placeholder="Add a more detailed description"
+              defaultValue={card.description || undefined}
+            />
+            <div className="flex items-center gap-x-2">
+              <FormSubmit variant="primary">Save</FormSubmit>
+              <Button
+                type="button"
+                size="sm"
+                variant="ghost"
+                onClick={disableEditing}
+              >
+                Cancel
+              </Button>
+            </div>
+          </form>
+        ) : (
+          <div
+            role="button"
+            onClick={enableEditing}
+            className="min-h-[78px] bg-neutral-200 text-sm font-medium py-3 px-3.5 rounded-md"
+          >
+            {card.description || "Add more detailed description ..."}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+Description.Skeleton = function DescriptionSkeleton() {
+  return (
+    <div className="flex items-center w-full gap-x-3">
+      <Skeleton className="size-6 bg-neutral-200" />
+      <div className="w-full">
+        <Skeleton className="w-24 h-6 mb-2 bg-neutral-200" />
+        <Skeleton className="w-full h-[78px] bg-neutral-200" />
+      </div>
+    </div>
+  );
+};
