95e8e300d51ee82618cb458745a3b7436198c867
finma <firmanmaulana2403@gmail.com>
2024-06-19 14:08:27 +0700

    [ADD] billing page

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/billing/page.tsx
@@ -0,0 +1,9 @@
+const BillingPage = () => {
+  return (
+    <div className="w-full flex items-center justify-center">
+      <p className="text-3xl text-neutral-600 font-medium">Coming Soon</p>
+    </div>
+  );
+};
+
+export default BillingPage;
