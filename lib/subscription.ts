875143d9d2f785f2c7faaac61228b27c4ab00c4e
finma <firmanmaulana2403@gmail.com>
2024-06-19 14:08:19 +0700

    [ADD] helper to check subscription

---
+++ lib/subscription.ts
@@ -0,0 +1,35 @@
+import { auth } from "@clerk/nextjs/server";
+
+import { db } from "@/lib/db";
+
+const DAY_IN_MS = 84_400_000;
+
+export const checkSubscription = async () => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    return false;
+  }
+
+  const orgSubscription = await db.orgSubscription.findUnique({
+    where: {
+      orgId,
+    },
+    select: {
+      stripeCustomerId: true,
+      stripeSubscriptionId: true,
+      stripePriceId: true,
+      stripeCurrentPeriodEnd: true,
+    },
+  });
+
+  if (!orgSubscription) {
+    return false;
+  }
+
+  const isValid =
+    orgSubscription.stripePriceId &&
+    orgSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS > Date.now();
+
+  return !!isValid;
+};
